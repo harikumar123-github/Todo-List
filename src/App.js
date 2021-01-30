1a78a2f069d5fa8f1062933ed74ed5c90aef53f4
harikumar <hari70319@gmail.com>
2021-01-30 10:51:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [list,setList] = useState([]);
+
+  const [newTask, setNew] = useState('');
+
+  const inputRef = useRef();
+
+  const addTask = () => {
+    if(newTask != null || newTask!== ''){
+      setList([...list,newTask]);
+      setNew("");
+      inputRef.current.value = "";
+    }
+  }
+
+  const deleteTask = val => {
+    setList( list.filter( ele => {
+        return ele !== val;
+      })
+    )
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className="heading"> Todo List </h1>
+
+      <div>
+
+        <input onKeyDown={ e => { if(e.keyCode===13) addTask() } } ref={inputRef} type="text" placeholder="Task to add" onChange={ e => { setNew(e.target.value) } }/>
+
+        <button onClick={addTask} > Add </button>
+
+      </div>
+
+      <hr />
+
+      <ul>
+        {list.map( (val,key) => {
+            return (
+              <div className='item'>
+                <li key={key}> {val} </li>
+                <button> &#10003; </button>
+                <button onClick={() => deleteTask(val) } > X </button>
+              </div>
+            )
+        } )}
+      </ul>
     </div>
   );
 }
